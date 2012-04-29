f09097816c29c9f69f1be89a037995a03ab85787
Erik Lundin <mjor.himself@gmail.com>
2012-04-29 09:42:11 +0200

    Simplified things a bit by using `Function#toString()` instead.

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-var data = "module.exports = function () { console.log(\"var data = \\\"%s\\\"\\n%s\", data.replace(/\\\\/g, \"\\\\\\\\\").replace(/\"/g, \"\\\\\\\"\"), data.replace(/{/, \"{\\n   \").replace(/}$/, \"\\n}\")); }"
 module.exports = function () {
-    console.log("var data = \"%s\"\n%s", data.replace(/\\/g, "\\\\").replace(/"/g, "\\\""), data.replace(/{/, "{\n   ").replace(/}$/, "\n}"));
+  console.log('module.exports = ' + arguments.callee.toString());
 }
